52e0567c1c1640fe31fec26f40856cba178632ee
eadortsu <eugeneadortsu@gmail.com>
2022-03-10 22:15:59 +0000

    fix 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	putchar(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	putchar(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
