dceef566fa47af314512d4b6c6b1ff10a69b1b0d
eadortsu <eugeneadortsu@gmail.com>
2022-03-10 22:38:47 +0000

    fix 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@ using namespace std;
  */
 int main(void)
 {
-       cout << "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+       putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
        return (1);
 }
