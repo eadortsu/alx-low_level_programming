b0ee1d48182a08312610a52bbb259819d34d0e1f
eadortsu <eugeneadortsu@gmail.com>
2022-03-11 20:03:23 +0000

    sdf

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,9 +12,13 @@ int main(void)
 	char low;
 
 	for (d = '0'; d <= '9'; d++)
+	{
 		putchar(d);
+		}
 	for (low = 'a'; low <= 'f'; low++)
+	{
 		putchar(low);
+		}
 		putchar('\n');
 
 	return (0);
