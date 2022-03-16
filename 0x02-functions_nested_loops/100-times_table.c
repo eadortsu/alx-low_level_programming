7c8930a048d1d80286135469b63e4f187baf2c1d
eadortsu <eugeneadortsu@gmail.com>
2022-03-16 05:55:38 +0000

    s

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,6 +6,34 @@
  */
 void print_times_table(int n)
 {
+int rone, cone, d;
+
+	for (rone = 0; rone <= n; rone++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (cone = 1; cone <= n; cone++)
+		{
+			d = (rone * cone);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (cone < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 
 	return (0);
 }
