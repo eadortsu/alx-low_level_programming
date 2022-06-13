face50cdb5cd8ba3e67218ecf41a637048629fa4
eadortsu <eugeneadortsu@gmail.com>
2022-06-13 22:30:05 +0000

    sd

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -q -O ../ https://github.com/eadortsu/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libmask.so?raw=true
-export LD_PRELOAD=libmask.so
+export LD_PRELOAD= ../libmask.so
